da8a56d1e2a58712efd7307cdf21e7e358ec02c4
Murilo Oliveira de Araujo <muritavo@outlook.com>
2017-06-27 01:05:07 -0300

    Feito

---
+++ src/java/jogodavelha3d/StatusJogo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogodavelha3d;
+
+/**
+ *
+ * @author murit
+ */
+public enum StatusJogo {
+    PLAYER_1_VENCEU, PLAYER_2_VENCEU, EM_JOGO, PLAYER_2_VENCEU_WO, PLAYER_1_VENCEU_WO
+    
+}
